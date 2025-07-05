5c66d25565c979ec5aa3d9b47f029305311eccc1
Cobalt <srozendom@gmail.com>
2025-07-05 16:02:59 +0200

    Comments

--- BAYAgent.py
+++ BAYAgent.py
@@ -27,6 +27,9 @@ class Agent:
 
         from pgmpy.factors.discrete import TabularCPD
 
+        # Create nodes
+
+        # Intended next direction
         cpd_user_intent = TabularCPD(
             variable="UserIntent",
             variable_card=4,  # Number of states: forwards, left, right, stop
@@ -34,6 +37,7 @@ class Agent:
             state_names={'UserIntent': ['Forwards', 'Left', 'Right', 'Stop']}
         )
 
+        # Direction that the agent is looking
         cpd_gaze_direction = TabularCPD(
             variable="GazeDirection",
             variable_card=5,  # Gaze can be forwards, left, right, backleft, backright
@@ -49,7 +53,7 @@ class Agent:
             state_names={'GazeDirection': ['GazeForwards', 'GazeLeft', 'GazeRight', 'GazeBackLeft', 'GazeBackRight'], 'UserIntent': ['Forwards', 'Left', 'Right', 'Stop']}
         )
 
-
+        # Direction that the agent moved in last time step
         cpd_previous_direction = TabularCPD(
             variable="PreviousDirection",
             variable_card=4,  # Forwards, Left, Right, Back
@@ -96,7 +100,7 @@ class Agent:
         # Store previous direction
         self.prev_dir = "Prev" + self.dir
 
-        # Estimate the most likely next state
+        # Estimate the most likely next direction
         self.EstimatedNextValue = max(self.nextArray)
         self.EstimatedNext = list(self.nextArray).index(self.EstimatedNextValue)
 
@@ -111,7 +115,7 @@ class Agent:
                            [self.position[0],self.position[1]-1]
                            ]
         
-        # determine estimation box direction
+        # determine estimation box heading
         if self.EstimatedNext == 1:
             self.EstimatedHeading = self.position[2]-1
             if self.EstimatedHeading < 0:
@@ -123,26 +127,28 @@ class Agent:
         else:
             self.EstimatedHeading = self.position[2]
         
-        # append estimation box
+        # append the coordinates of the apropriate spots in the extimated heading 
 
         if self.EstimatedHeading == 0:
-            self.boundryBox.append([self.position[0],self.position[1]+2])
-            self.boundryBox.append([self.position[0]+1,self.position[1]+1])
-            self.boundryBox.append([self.position[0]-1,self.position[1]+1])
+            self.boundryBox.append([self.position[0],self.position[1]+2])       #     O
+            self.boundryBox.append([self.position[0]+1,self.position[1]+1])     #   O   O 
+            self.boundryBox.append([self.position[0]-1,self.position[1]+1])     #     X
         elif self.EstimatedHeading == 1:
-            self.boundryBox.append([self.position[0]+1,self.position[1]+1])
-            self.boundryBox.append([self.position[0]+2,self.position[1]])
-            self.boundryBox.append([self.position[0]+1,self.position[1]-1])
+            self.boundryBox.append([self.position[0]+1,self.position[1]+1])     #    O
+            self.boundryBox.append([self.position[0]+2,self.position[1]])       #   X  O
+            self.boundryBox.append([self.position[0]+1,self.position[1]-1])     #    O  
         elif self.EstimatedHeading == 2:
-            self.boundryBox.append([self.position[0],self.position[1]-2])
-            self.boundryBox.append([self.position[0]-1,self.position[1]-1])
-            self.boundryBox.append([self.position[0]+1,self.position[1]-1])
+            self.boundryBox.append([self.position[0],self.position[1]-2])       #     X
+            self.boundryBox.append([self.position[0]-1,self.position[1]-1])     #   O   O
+            self.boundryBox.append([self.position[0]+1,self.position[1]-1])     #     O
         elif self.EstimatedHeading == 3:
-            self.boundryBox.append([self.position[0]-1,self.position[1]+1])
-            self.boundryBox.append([self.position[0]-2,self.position[1]])
-            self.boundryBox.append([self.position[0]-1,self.position[1]-1])
+            self.boundryBox.append([self.position[0]-1,self.position[1]+1])     #     O
+            self.boundryBox.append([self.position[0]-2,self.position[1]])       #   O   X
+            self.boundryBox.append([self.position[0]-1,self.position[1]-1])     #     O
         else:
             print("Error: undefined estimated rotation")
+        
+        # Wrap around the boundary boxes to the other side of the square
         for i in range(len(self.boundryBox)):
             for j in range(len(self.boundryBox[i])):
                 if self.boundryBox[i][j] < 0:
@@ -153,6 +159,7 @@ class Agent:
         return self.boundryBox
     
     def set_boundries(self,boundries):
+        # Unpack the list of boundaries per agent into a list of all coordinates this agent is not allowed to go to
         self.nogozone = []
         for i in range(len(boundries)):
             for j in range(len(boundries[i])):
@@ -165,6 +172,7 @@ class Agent:
         picker = random.random()
         self.nextPosition = [self.position[0],self.position[1],self.position[2]]
 
+        # Turn direction into heading
         if picker <= self.nextArray[0]:
             self.dir = "Forwards"
         elif picker <= self.nextArray[0] + self.nextArray[1]:
@@ -180,13 +188,15 @@ class Agent:
         else:
             self.dir = "Stop"        
         
-        # break recursion
+        # break recursion (started when trying to walk into a no-go-zone to find a different direction)
         if self.depth > 10:
             self.dir = "Stop"
             self.depth = 0
 
+        # Print the direction the agent will try to walk into
         print("RealDir " + self.dir)
 
+        # Determine next position based on heading
         if self.dir != "Stop":
             if self.position[2] == 0:
                 self.nextPosition[1] = self.position[1] +1
@@ -214,9 +224,12 @@ class Agent:
             self.depth = self.depth + 1
             if self.dir != "Stop":
                 print("Deciding: " + str(self.depth))
-                self.rebalanceValues(self.dir)
+                # Ajust the result from the baysian network to remove the direction that did not work
+                self.rebalanceValues(self.dir) 
+        
                 self.do_next_step()
         
+        # Confirm new position
         self.position[0] = self.nextPosition[0]
         self.position[1] = self.nextPosition[1]
         self.position[2] = self.nextPosition[2]
